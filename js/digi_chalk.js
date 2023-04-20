3aa732ce334e9592bdb3fae461a9dc2768c64c81
marleywilliford <129218190+marleywilliford@users.noreply.github.com>
2023-04-20 11:20:46 -0500

    New JS functions
    Added lastItem() and alphabetize() functions

---
+++ js/digi_chalk.js
@@ -0,0 +1,21 @@
+
+
+function lastItem(arr) {
+    document.getElementById('originalArray').innerHTML = "Original array: " + JSON.stringify(arr);
+    var lastItem = arr.sort()[arr.length - 1];
+    document.getElementById('lastAlphabetically').innerHTML += "Last item: " + lastItem;
+    console.log(lastItem);
+}
+
+function alphabetize() {
+    var fruit = document.getElementById('fruit').value;
+    var animal = document.getElementById('animal').value;
+    var state = document.getElementById('state').value;
+    var country = document.getElementById('country').value;
+
+    var alphaOrig = [fruit, animal, state, country];
+    document.getElementById('alphaOrig').innerHTML = "Original order: " + JSON.stringify(alphaOrig);
+    
+    var alphaOutput = alphaOrig.slice().sort()
+    document.getElementById('alphaOutput').innerHTML = "Alphabetical order: " + JSON.stringify(alphaOutput);
+}
